ee4c525eba17156830b97fff4af76b639034a65e
aabdelaziz92 <a.abdelaziz44@gmail.com>
2025-07-03 02:13:22 +0300

    Add environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiKey: 'da6cc3d2a21b451276732aa6'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiKey: 'da6cc3d2a21b451276732aa6'
+}
