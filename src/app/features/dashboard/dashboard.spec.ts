6e348db356b3feaca18c1a06a669a10f70d3879b
aabdelaziz92 <a.abdelaziz44@gmail.com>
2025-07-03 05:09:11 +0300

    Write tests for dashboard and add expense components

--- src/app/features/dashboard/dashboard.spec.ts
+++ src/app/features/dashboard/dashboard.spec.ts
@@ -1,23 +1,117 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Dashboard } from './dashboard';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { ListOfExpenses } from '../expense/list-of-expenses/list-of-expenses';
+import { LucideAngularModule } from 'lucide-angular';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthStore } from '../../core/services/auth.store';
+import { ExpensesStore } from '../../core/services/expenses.store';
+import { BehaviorSubject, of } from 'rxjs';
+import { Expense } from '../../core/models/expense';
 
-describe('Dashboard', () => {
+fdescribe('Dashboard', () => {
   let component: Dashboard;
   let fixture: ComponentFixture<Dashboard>;
+  let authStoreSpy: jasmine.SpyObj<AuthStore>;
+  let expensesStoreSpy: jasmine.SpyObj<ExpensesStore>;
+
+  const mockExpenses: Expense[] = [
+    { id: '1', amount: 100, date: new Date('2024-01-01'), category: 'food', currency: 'egp', attachment: '', icon: '', color: '' },
+    { id: '2', amount: 200, date: new Date('2024-01-02'), category: 'transport', currency: 'egp', attachment: '', icon: '', color: '' },
+  ];
+
+  const mockIncome: Expense[] = [
+    { id: '1', amount: 1000, date: new Date('2024-01-01'), category: 'salary', currency: 'egp', attachment: '', icon: '', color: '' },
+  ];
 
   beforeEach(async () => {
+    const authStoreMock = jasmine.createSpyObj('AuthStore', ['user$']);
+    // authStoreMock.user$.and.returnValue(of({
+    //   name: 'Ahmed Abdelaziz',
+    //   pictureUrl: 'https://example.com/avatar.jpg'
+    // }));
+
+    const expensesStoreMock = jasmine.createSpyObj('ExpensesStore', [
+      'incomeExpenses',
+      'outcomeExpenses',
+      'getFilteredExpenses',
+      'setCurrency'
+    ]);
+    // expensesStoreMock.expenses$.and.returnValue(of(mockExpenses));
+    const income$ = new BehaviorSubject(mockIncome);
+    const outcome$ = new BehaviorSubject(mockExpenses);
+    const rate$ = new BehaviorSubject(30);
+    expensesStoreMock.incomeExpenses.and.returnValue(income$);
+    expensesStoreMock.outcomeExpenses.and.returnValue(outcome$);
+    expensesStoreMock.getFilteredExpenses.and.returnValue(of(mockExpenses));
+    expensesStoreMock.rate$ = rate$;
+
     await TestBed.configureTestingModule({
-      imports: [Dashboard]
+      imports: [
+        Dashboard
+      ],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        provideHttpClient(),
+        { provide: AuthStore, useValue: authStoreMock },
+        { provide: ExpensesStore, useValue: expensesStoreMock }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Dashboard);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authStoreSpy = TestBed.inject(AuthStore) as jasmine.SpyObj<AuthStore>;
+    expensesStoreSpy = TestBed.inject(ExpensesStore) as jasmine.SpyObj<ExpensesStore>;
+    fixture.whenStable();
   });
 
-  it('should create', () => {
+  it('should create the dashboard component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update currency using setCurrency when currency changes', async () => {
+    component.dashboardForm.get('currency')?.setValue('usd');
+    component.dashboardForm.get('currency')?.valueChanges.subscribe(value => {
+      expect(value).toBe('usd');
+      expect(expensesStoreSpy.setCurrency).toHaveBeenCalledWith('usd');
+    })
+  });
+
+  it('should calculate total income amount using exchange rate', async () => {
+    component.getIncomeAmount().subscribe(total => {
+      // 1000 * 30 = 30000
+      expect(total).toBe(30000);
+    });
+  });
+
+  it('should calculate total expense amount using exchange rate', async () => {
+    component.getExpenseAmount().subscribe(total => {
+      // (100 + 200) * 30 = 300 * 30
+      expect(total).toBe(9000);
+    });
+  });
+
+  it('should calculate total balance correctly', async () => {
+    component.getTotalBalance().subscribe(balance => {
+      // income: 1000 * 30 = 30000
+      // expense: (100+200) * 30 = 9000
+      expect(balance).toBe(21000);
+    });
+  });
+
+  it('should apply exchange rate to filtered expenses in getListOfExpenses()', async () => {
+    component.getListOfExpenses('all').subscribe(expenses => {
+      expect(expenses.length).toBe(2);
+      expect(expenses[0].amount).toBe(100 * 30);
+      expect(expenses[1].amount).toBe(200 * 30);
+    });
+  });
+
+
 });
--- src/app/features/expense/add-expense/add-expense.spec.ts
+++ src/app/features/expense/add-expense/add-expense.spec.ts
@@ -1,23 +1,98 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AddExpense } from './add-expense';
+import { ExpensesStore } from '../../../core/services/expenses.store';
+import { Category } from '../../../core/models/category';
+import { of } from 'rxjs';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { Dashboard } from '../../dashboard/dashboard';
+import { By } from '@angular/platform-browser';
 
-describe('AddExpense', () => {
+
+fdescribe('AddExpense', () => {
   let component: AddExpense;
   let fixture: ComponentFixture<AddExpense>;
+  let expensesStoreSpy: jasmine.SpyObj<ExpensesStore>;
+
+  const mockCategories: Category[] = [
+    { id: '1', value: 'food', label: 'Food', icon: 'pizza', color: '#DCDAE8', selected: false },
+    { id: '2', value: 'gift', label: 'Gift', icon: 'gift', color: '#DCDAE8', selected: false },
+    { id: '3', value: 'transport', label: 'Transport', icon: 'car', color: '#D9C2EA', selected: false },
+    { id: '4', value: 'rent', label: 'Rent', icon: 'house', color: '#CFB183', selected: false },
+    { id: '5', value: 'shopping', label: 'Shopping', icon: 'ShoppingCart', color: '#EABF74', selected: false },
+    { id: '6', value: 'entertainment', label: 'Entertainment', icon: 'FerrisWheel', color: '#1C57F0', selected: false },
+    { id: '7', value: 'fuel', label: 'Fuel', icon: 'fuel', color: '#E39E9E', selected: false },
+    { id: '8', value: 'salary', label: 'Salary', icon: 'BanknoteArrowUp', color: '#E39E9E', selected: false },
+    { id: '9', value: 'other', label: 'Other', icon: 'shapes', color: '#808080', selected: false }
+  ];
 
   beforeEach(async () => {
+    const mockExpensesStore = jasmine.createSpyObj('ExpensesStore', ['getAllCategories', 'addExpense']);
+    mockExpensesStore.getAllCategories.and.returnValue(of(mockCategories));
+
     await TestBed.configureTestingModule({
-      imports: [AddExpense]
+      imports: [AddExpense],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideRouter([
+          { path: 'dashboard', component: Dashboard }
+        ]),
+        // { provide: Router, useValue: mockRouter }, // This is not working and keeps throwing an error ==> The thing is when you import (provideRouter/RouterTestingModule) you should remove all router mocked providers
+        { provide: ExpensesStore, useValue: mockExpensesStore }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AddExpense);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    expensesStoreSpy = TestBed.inject(ExpensesStore) as jasmine.SpyObj<ExpensesStore>;
+    fixture.whenStable();
   });
 
-  it('should create', () => {
+  it('should create the add expense component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create the form with default values', () => {
+    expect(component.addForm).toBeDefined();
+    expect(component.addForm.valid).toBeFalse();
+    expect(component.addForm.get('currency')?.value).toBe('egp');
+  });
+
+  it('should mark form as invalid if required fields are missing', () => {
+    component.addForm.patchValue({
+      amount: '',
+      category: '',
+      date: ''
+    });
+
+    expect(component.addForm.invalid).toBeTrue();
+    expect(component.addForm.get('amount')?.hasError('required')).toBeTrue();
+    expect(component.addForm.get('category')?.hasError('required')).toBeTrue();
+  });
+
+  it('should validate amount as numeric and greater than 0', () => {
+    component.addForm.get('amount')?.setValue('abc');
+    expect(component.addForm.get('amount')?.valid).toBeFalse();
+    expect(component.addForm.get('amount')?.hasError('pattern')).toBeTrue();
+
+    component.addForm.get('amount')?.setValue(0);
+    expect(component.addForm.get('amount')?.hasError('min')).toBeTrue();
+
+    component.addForm.get('amount')?.setValue(10);
+    expect(component.addForm.get('amount')?.valid).toBeTrue();
+  });
+
+  it('should not call addExpense if form is invalid', async () => {
+    component.addForm.patchValue({
+      category: '',
+      amount: '',
+      date: ''
+    });
+
+    component.addExpense();
+
+    expect(expensesStoreSpy.addExpense).not.toHaveBeenCalled();
   });
 });
